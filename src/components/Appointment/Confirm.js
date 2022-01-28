87ae96ae556682c8fb33e7f398c58926f310b8a2
afif <afifadelayoub@gmail.com>
2022-01-28 02:23:12 -0500

    project submitted

--- src/components/Appointment/Confirm.js
+++ src/components/Appointment/Confirm.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Button from "components/Button";
 
+//this is a function that is exported, it is called confirm, an argument that is given is called props
 export default function Confirm(props) {
+  //this is the return
   return (
     <main className="appointment__card appointment__card--confirm">
       <h1 className="text--semi-bold">Delete the appointment?</h1>
       <section className="appointment__actions">
-        <Button danger onClick={props.onCancel}>Cancel</Button>
-        <Button danger onClick={props.onConfirm}>Confirm</Button>
+        <Button danger onClick={props.onCancel}>
+          Cancel
+        </Button>
+        <Button danger onClick={props.onConfirm}>
+          Confirm
+        </Button>
       </section>
     </main>
-  )
+  );
 }
--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,16 +5,13 @@ import { useState } from "react";
 import { validate } from "@babel/types";
 import Button from "components/Button";
 
-
 export default function Form(props) {
-
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.value || null);
   const [error, setError] = useState("");
   function reset() {
-
     setStudent("");
-    setInterviewer(null)
+    setInterviewer(null);
   }
 
   function cancel() {
@@ -23,40 +20,59 @@ export default function Form(props) {
   }
 
   function validate() {
-
-    if (student === "") {
-      setError("Appointments must have a student name and an interviewer")
+    if (interviewer === "") {
+      setError("Appointments must have a student name and an interviewer");
       return;
-    } 
-    setError("")
-    props.onSave(student, interviewer);
     }
-  
+    setError("");
+    props.onSave(student, interviewer);
+  }
 
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer);
+  }
 
   return (
-<main className="appointment__card appointment__card--create">
-  <section className="appointment__card-left">
-    <form autoComplete="off" onSubmit={event => event.preventDefault()}>
-      <input
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
+          <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             value={student}
             placeholder="Enter Student Name"
             onChange={(event) => setStudent(event.target.value)}
-      />
-    </form>
-    <InterviewerList 
-          interviewers={props.interviewers} value={interviewer} interviewer={interviewer} onChange={(event) => setInterviewer(event)}
-    />
-  </section>
-  <section className="appointment__card-right">
-    <section className="appointment__actions">
-      <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onSubmit={event => event.preventDefault()} onClick={validate}>Save</Button>
-    </section>
-  </section>
-</main>
-  )
+            data-testid="student-name-input"
+          />
+        </form>
+        <section className="appointment__validation">{error}</section>
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          interviewer={interviewer}
+          onChange={(event) => setInterviewer(event)}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button
+            confirm
+            onSubmit={(event) => event.preventDefault()}
+            onClick={validate}
+          >
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
 }
